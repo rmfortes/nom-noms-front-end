a33d19e62241858fba86806b4b920aa238d1784d
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-04-21 12:34:33 -0400

    wireframe built using react

---
+++ src/Body.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Body extends Component {
+  render() {
+    return (
+      <div className="body">
+        <div className="searchBar">
+          <p>This is where our search bar will go.</p>
+        </div>
+        <div className="results">
+            <p>This is where our search results and recipes will go.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Body;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Header() {
+  return (
+    <div>
+      <header className="header">
+          <p>NomNoms</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
