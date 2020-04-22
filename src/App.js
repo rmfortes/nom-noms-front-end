bfe74ac148f106c7d46b595966ccbdc2cc13193b
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-04-22 17:40:54 -0400

    username and password forms rendering on page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 import Header from './Header.js';
 import Body from './Body.js';
+import Forms from './Forms.js';
+import Buttons from './Buttons.js';
+import CreateUser from './CreateUser.js';
+
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header/>
-      <Body/>
+      
+     <Switch>
+      <Link>
+    <Route path = "/" exact component={Forms}></Route>
+    </Link>
+    {Buttons}
+    {CreateUser}
+      </Switch>
+      
+  
     </div>
+    </Router>
   );
 }
 
