d060704a5ee15b515228a029b3d8bbccbaea6ccb
Athly23 <andrewly06@gmail.com>
2020-04-23 14:00:12 -0400

    added code to forms.js and createuser.js

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,12 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
 
-const CreateUser = props => (
-  <form className={`forms-${props.type}`}>
-    {" "}
-    <input type="text" placeholder="Create Username" ></input>
-    <input type="text" placeholder="Create Password" ></input>
-    <input type="submit" ></input>
-  </form>
-);
+class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      createUsername: "",
+      createPassword: "",
+    };
+    this.usernameCreated = this.usernameCreated.bind(this);
+    this.passwordCreated = this.passwordCreated.bind(this);
+  }
+
+  usernameCreated = e => {
+    e.preventDefault();
+    console.log(e.target.value);
+    this.setState({
+      username: e.target.username,
+    });
+
+    //this.setState { username: e.target.value}
+  };
+
+  passwordCreated = e => {
+    e.preventDefault();
+    console.log(e.target.value);
+    this.setState({
+      password: e.target.password,
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.value);
+    let URL = `http://localhost:4000/user`;
+    fetch(URL, {
+      method: "post",
+      body: JSON.stringify(this.state), //converts joke object to JSON, then send that JSON to post requset
+      headers: { "Content-type": "application/json" },
+    });
+    // fetch(URL)
+    //   .then(res => res.json())
+    //   .then(res => console.log(res));
+
+    //fetch here
+  };
+  render() {
+    return (
+      <div>
+        <form className={`forms-${this.props.type}`}>
+          {" "}
+          <input
+            type="text"
+            placeholder="Create Username"
+            value={this.state.createUsername}
+            onChange={this.usernameCreated}
+          ></input>
+          <input
+            type="text"
+            placeholder="Create Password"
+            value={this.state.createPassword}
+            onChange={this.passwordCreated}
+          ></input>
+          <input
+            onClick={this.handleSubmit}
+            type="Submit"
+            placeholder="submit"
+          ></input>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default CreateUser;
