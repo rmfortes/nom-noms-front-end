6e0cd39e4bbe3f2edc62437a1f46172865c16a04
Athly23 <andrewly06@gmail.com>
2020-04-23 23:49:18 -0400

    added code for the favorites buttons that will return all recipes that was favorited by the user

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Buttons.css";
 import Forms from "./Forms";
 
-const Buttons = props => {
-  let classList = "";
+import React, { Component } from "react";
 
-  let types = ["login", "submit", "favorites"];
+class Buttons extends Component {
+  constructor(props) {
+    super(props);
 
-  let onSubmit = () => {
-    console.log("I've been clicked!");
+    this.state = {
+      Name: "",
+      Ingredients: [],
+      URL: "",
+    };
   }
 
-  if (types.includes(props.type)) {
-    classList += ` button-${props.type}`;
+  handleClick = e => {
+    e.preventDefualt();
+    console.log("I have been click");
+    let URL = `https://nom-noms-api.herokuapp.com/search`;
+    fetch(URL, {
+      method: "GET",
+      body: JSON.stringify(this.state),
+      headers: { "Content-type": "application/json" },
+    }).catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <button className={classList} onClick={this.handleClick}>
+          {this.props.label}
+        </button>
+      </div>
+    );
   }
-
-  return <button className={classList} onClick={props.onClick}>{props.label}</button>;
-
-
-};
+}
 
 export default Buttons;
+
+// const Buttons = props => {
+//   let classList = "";
+
+//   let types = ["login", "submit", "favorites"];
+
+//   let onClick = () => {
+//     console.log("I've been clicked!");
+//   }
+
+//   if (types.includes(props.type)) {
+//     classList += ` button-${props.type}`;
+//   }
+
+//   return <button className={classList} onClick={props.onClick}>{props.label}</button>;
+
+// };
+
+// export default Buttons;
 
 // function handleSubmit(e) {
 //   e.preventDefault()
