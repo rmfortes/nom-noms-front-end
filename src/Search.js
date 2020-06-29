3222783f430c6bdd48dcb71d7e5cb53de8930d6e
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-06-29 16:03:09 -0400

    fixed keyword search functionality on front end and it is now displaying the results

--- src/Search.js
+++ src/Search.js
@@ -5,7 +5,8 @@ class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ""
+      searchTerm: "",
+      searchResults: [],
     };
   }
 
@@ -14,23 +15,54 @@ class Search extends Component {
 
     this.setState({
       searchTerm: e.target.value,
-      //We are not setting the state of the username, we are setting the state of the body? Of what are we setting the state? 
+      //We are not setting the state of the username, we are setting the state of the body? Of what are we setting the state?
     });
-    console.log(this.state.searchTerm)
+    console.log(this.state.searchTerm);
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
     console.log(this.state);
     let URL = `https://nom-noms-api.herokuapp.com/search/?ingredient=${this.state.searchTerm}`;
-    fetch(URL, {
-      method: "GET",
-      body: JSON.stringify(this.state),
-      headers: { "Content-type": "application/json" },
-    }).catch((err) => console.log(err));
+    fetch(URL)
+      .then((res) => res.json())
+      .then((searchData) => {
+        console.log(searchData);
+        this.setState({
+          searchResults: searchData,
+        });
+      });
   };
 
+  //     method: "GET",
+  //     body: JSON.stringify(this.state),
+  //     headers: { "Content-type": "application/json" },
+  //   }).catch((err) => console.log(err));
+  // };
+  // fetch(searchURL)
+  //   .then((res) => res.json())
+  //   .then((searchData) => {
+  //     console.log(searchData);
+  //     let searchResults = searchData.map((search) => ({
+  //       searchTerm: `${searchTerm}`,
+  //     }));
+  //     console.log(searchResults);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
   render() {
+    let recipes = this.state.searchResults.map((recipe, i) => {
+      return (
+        <div className="searchResults" key={i}>
+          <ul>
+            {recipe.Name} <br></br>
+            {recipe.URL}
+          </ul>
+        </div>
+      );
+    });
     return (
       <div>
         <form
@@ -48,6 +80,9 @@ class Search extends Component {
             placeholder="submit"
           ></input>
         </form>
+        <div className="searchResults">
+          Search Results: {recipes} <br></br>
+        </div>
       </div>
     );
   }
