2a9dbd3956efa4f2288fce976b30a079c7fafd20
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-07-17 11:54:21 -0400

    styled splash page

--- src/pages/Splash/Splash.js
+++ src/pages/Splash/Splash.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './Splash.css';
 
-function Splash(props){
+function Splash(props) {
 
-    return(
+    return (
         <div id="splashdiv" className="splashdiv">
-            <h1>NomNoms</h1>
-            <h2>Login</h2>
-            <input type="text"></input>
-            <input type="text"></input>
-            <h2>Register</h2>
-            <input type="text"></input>
-            <input type="text"></input>
-            <input type="text"></input>
+            <div className="splashinner">
+                <h1>NomNoms</h1>
+                <div className="input">
+                    <h2>Login</h2>
+                    <input type="text"></input>
+                    <input type="text"></input>
+                    <button>Login</button>
+                </div>
+                <div className="input">
+                    <h2>Register</h2>
+                    <input type="text"></input>
+                    <input type="text"></input>
+                    <input type="text"></input>
+                    <button>Get Started</button>
+                </div>
+            </div>
         </div>
-        
     )
 
 }
