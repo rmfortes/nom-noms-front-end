b351fdf62af4ad006f5da300f991e520ce650b44
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-07-17 11:28:18 -0400

    reset app and started building new pages

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-import Header from "./Header.js";
-import Forms from "./Forms.js";
-import CreateUser from "./CreateUser.js";
+import React from 'react';
+import Splash from './pages/Splash/Splash';
+import './App.css';
 
-import Body from "./Body";
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      Username: "",
-      Password: "",
-      profiles: [],
-    };
-    this.setProfiles = this.setProfiles.bind(this);
-  }
-  setProfiles(profiles) {
-    this.setState({ profiles: profiles });
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Header />
-          <div className="login">
-            <Route path="/" exact component={Forms} />
-            <Route path="/" exact component={CreateUser} />
-
-            <Route
-              path="/user"
-              render={(routerProps) => (
-                <Body
-                  setProfiles={this.setProfiles}
-                  addUser={this.usernameCreated}
-                  {...routerProps}
-                  {...this.state}
-                />
-              )}
-            />
-          </div>
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Splash>
+        
+      </Splash>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Splash/Splash.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Splash.css';
+
+function Splash(props){
+
+    return(
+        <div id="splashdiv" className="splashdiv">
+            <h1>NomNoms</h1>
+            <h2>Login</h2>
+            <input type="text"></input>
+            <input type="text"></input>
+            <h2>Register</h2>
+            <input type="text"></input>
+            <input type="text"></input>
+            <input type="text"></input>
+        </div>
+        
+    )
+
+}
+export default Splash;
