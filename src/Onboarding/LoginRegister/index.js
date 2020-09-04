f2cf3295f81ae708bd3fe674653536b6f8cf1590
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-09-04 19:42:06 -0400

    initial programming of register div and some styling, as well as setting css standard rules for site

--- src/Onboarding/LoginRegister/index.js
+++ src/Onboarding/LoginRegister/index.js
@@ -4,9 +4,21 @@ import './style.css';
 const LoginRegister = () => {
     return(
         <div className = "register-div">
-            Welcome to NomNoms!
-            Please Login to continue.
-            Please Register if you do not have an account
+            <div className="welcome-message">
+                <h1>Welcome to NomNoms!</h1>
+            </div>
+            <div className="login">
+                <h3>Login</h3>
+                <form type="submit">
+                    <input type="text"></input>
+                    <input type="text"></input>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+            <div className="register">
+                <h3>Register New Account</h3>
+                <button>Register</button>
+            </div>
         </div>
     )
 }
